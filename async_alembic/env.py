e1f1618bbfcc386b2e59d9acf4338efd00e24dd8
Alex Pavlov <yukiu217@gmail.com>
2024-12-16 21:52:33 +0600

    alembic settings

--- async_alembic/env.py
+++ async_alembic/env.py
@@ -3,7 +3,7 @@ from logging.config import fileConfig
 
 from sqlalchemy import pool
 from sqlalchemy.engine import Connection
-from sqlalchemy.ext.asyncio import async_engine_from_config, AsyncEngine
+from sqlalchemy.ext.asyncio import AsyncEngine
 from sqlalchemy import engine_from_config
 
 from alembic import context
